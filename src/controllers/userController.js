fea14e68fa7e01d88025d5b64932a9b75c4a270d
groteskzp <groteskzp@gmail.com>
2026-09-24 17:37:43 +0300

    feat: get user locations with pagination

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,8 @@
-import { notImplemented } from '../utils/notImplemented.js';
+// src/controllers/userController.js
+import createHttpError from "http-errors";
+import { notImplemented } from "../utils/notImplemented.js";
+import { User } from "../models/user.js";
+import { Location } from "../models/location.js";
 
 export const getCurrentUser = async (req, res) => {
   res.status(200).json(req.user);
@@ -6,4 +10,34 @@ export const getCurrentUser = async (req, res) => {
 export const updateCurrentUser = notImplemented;
 export const updateUserAvatar = notImplemented;
 export const getUserById = notImplemented;
-export const getUserLocations = notImplemented;
+
+export const getUserLocations = async (req, res) => {
+  const { userId } = req.params;
+  const { page, limit } = req.query;
+
+  const userExists = await User.exists({ _id: userId });
+  if (!userExists) {
+    throw createHttpError(404, "User not found");
+  }
+
+  const filter = { owner: userId };
+  const skip = (page - 1) * limit;
+
+  const [total, data] = await Promise.all([
+    Location.countDocuments(filter),
+    Location.find(filter)
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("type", "name")
+      .populate("region", "name"),
+  ]);
+
+  res.json({
+    data,
+    page,
+    limit,
+    total,
+    totalPages: Math.ceil(total / limit),
+  });
+};
