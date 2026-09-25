8597804144a15c63dcac99d2c86ffd7b49cb84ac
zhenya-kr <zhekakrochak79@gmail.com>
2026-09-25 18:01:11 +0200

    create location controller

--- src/controllers/locationController.js
+++ src/controllers/locationController.js
@@ -1,6 +1,11 @@
+import { Location } from '../models/location.js';
 import { notImplemented } from '../utils/notImplemented.js';
 
 export const getLocations = notImplemented;
 export const getLocationById = notImplemented;
-export const createLocation = notImplemented;
+export const createLocation = async (req, res) => {
+  const location = await Location.create(req.body);
+
+  res.status(201).json(location);
+};
 export const updateLocation = notImplemented;
