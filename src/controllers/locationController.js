2947146f31abc9106735717d074f231b869f7293
zhenya-kr <zhekakrochak79@gmail.com>
2026-09-26 16:57:21 +0200

    controller create location

--- src/controllers/locationController.js
+++ src/controllers/locationController.js
@@ -1,10 +1,28 @@
+import createHttpError from 'http-errors';
 import { Location } from '../models/location.js';
 import { notImplemented } from '../utils/notImplemented.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
 export const getLocations = notImplemented;
 export const getLocationById = notImplemented;
 export const createLocation = async (req, res) => {
-  const location = await Location.create(req.body);
+  const { name, type, region, description } = req.body;
+
+  if (!req.files?.length) {
+    throw createHttpError(400, 'At least one image is required');
+  }
+
+  const images = await Promise.all(
+    req.files.map((file) => saveFileToCloudinary(file, 'locations')),
+  );
+  const location = await Location.create({
+    name,
+    type,
+    region,
+    description,
+    images,
+    owner: req.user._id,
+  });
 
   res.status(201).json(location);
 };
